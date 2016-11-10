f6cf26aa88a9e074db9575fed6a00b97b3224e13
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-10 12:21:40 +0100

    add adding functionality

--- src/pl/akademiakodu/git/Calc.java
+++ src/pl/akademiakodu/git/Calc.java
@@ -7,7 +7,10 @@ public class Calc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println("Suma: " + (a + b));
 	}
 
 }

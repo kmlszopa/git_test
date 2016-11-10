eda47ebd926f031025b52f7a43f90a3b96c7bbb0
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-10 11:52:23 +0100

    Revert "added scanner to my class"
    This reverts commit 472c5f89d5457cc4e1f064fef02acf9ce1d6d3a0.

--- src/pl/akademiakodu/git/Calc.java
+++ src/pl/akademiakodu/git/Calc.java
@@ -1,12 +1,9 @@
 package pl.akademiakodu.git;
 
-import java.util.Scanner;
-
 public class Calc {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
+		// TODO Auto-generated method stub
 
 	}
 

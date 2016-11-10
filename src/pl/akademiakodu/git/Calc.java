472c5f89d5457cc4e1f064fef02acf9ce1d6d3a0
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-10 11:40:44 +0100

    added scanner to my class

--- src/pl/akademiakodu/git/Calc.java
+++ src/pl/akademiakodu/git/Calc.java
@@ -1,9 +1,12 @@
 package pl.akademiakodu.git;
 
+import java.util.Scanner;
+
 public class Calc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
 
 	}
 
